82f89daf3d8f97b1ef78f9827908e12d4f645008
João Sanguedo <JoaoSangue@users.noreply.github.com>
2023-08-12 13:43:17 -0300

    optimizes persistence

--- odontolab/model/patient/patient.py
+++ odontolab/model/patient/patient.py
@@ -1,16 +1,17 @@
-from datetime import date
+from datetime import date, datetime
 
 class Patient:
-    def __init__(self, cpf: str, name: str, birthdate: int, addr: str, phone: str):
-        self.id: int
+    def __init__(self, cpf: str = '', name: str = '', birthdate: str = '', addr: str = '', phone: str = ''):
+        self.id: int = 0
 
         self.cpf: str = cpf
         self.name: str = name
-        self.birthdate: date = date.fromtimestamp(birthdate)
+        self.birthdate: date = datetime.strptime(birthdate, "%Y-%m-%d").date()
         self.addr: str = addr
         self.phone: str = phone
     
     def __isValid(self) -> bool:
+        # TODO: Consider viability of wrapper class for validating specific strings 
         if self.cpf.strip() == '':
             print('Invalid cpf for patient')
             return False
@@ -26,6 +27,8 @@ class Patient:
         if self.phone.strip() == '':
             print('Invalid phone for patient')
             return False
+        
+        return True
 
     def canBeCreated(self) -> bool:
         return self.__isValid()
--- odontolab/model/patient/persistence.py
+++ odontolab/model/patient/persistence.py
@@ -9,18 +9,21 @@ class Persistence(PatientDAO, metaclass=SingletonMeta):
 
     def __init__(self):
         self.__patients: dict[int, Patient] = dict()
+        self.__patients_cpf_index: dict[str, int] = dict()
 
     def create(self, patient: Patient) -> tuple[Patient, bool]:
-        if patient.id not in self.__patients:
+        if patient.id not in self.__patients and patient.cpf not in self.__patients_cpf_index:
             patient.id = len(self.__patients) + 1
             self.__patients[patient.id] = patient
+            self.__patients_cpf_index[patient.cpf] = patient.id
             return patient, True
 
         return patient, False
     
     def find(self, cpf: str) -> tuple[Patient, bool]:
-        for patient in self.__patients.values():
-            if patient.cpf == cpf:
-                return patient, True
+        if cpf in self.__patients_cpf_index:
+            id = self.__patients_cpf_index[cpf]
+            patient = self.__patients[id]
+            return patient, True
 
-        return  Patient('', '', 0, '', ''), False
+        return  Patient(), False
